0c717fd61804352d2c158536778feee6316c4bfa
Mitsuki <26341548+mmm3w@users.noreply.github.com>
2022-03-22 16:26:43 +0800

    update)header ...

--- src/main/java/com/mitsuki/armory/httprookie/request/Header.kt
+++ src/main/java/com/mitsuki/armory/httprookie/request/Header.kt
@@ -1,31 +1,62 @@
 package com.mitsuki.armory.httprookie.request
 
+import java.util.*
+import kotlin.collections.LinkedHashMap
+
 interface Headers {
-    val headers: LinkedHashMap<String, String>
+    val headers: LinkedHashMap<String, LinkedList<String>>
 
     companion object {
         const val HEAD_CONTENT_LENGTH = "Content-Length"
     }
 }
 
-fun Headers.header(key: String, value: String) {
+fun Headers.header(key: String, value: String, replace: Boolean = false) {
     if (key.isEmpty()) return
     if (value.isEmpty()) return
-    headers[key] = value
+    if (replace) {
+        headers[key] = LinkedList<String>().apply { add(value) }
+    } else {
+        (headers[key] ?: LinkedList<String>().apply { headers[key] = this }).add(value)
+    }
 }
 
-fun Headers.header(data: Headers) {
+fun Headers.header(key: String, value: List<String>, replace: Boolean = false) {
+    if (key.isEmpty()) return
+    if (value.isEmpty()) return
+    if (replace) {
+        headers[key] = LinkedList<String>(value)
+    } else {
+        headers[key]?.apply { addAll(value) } ?: let {
+            headers[key] = LinkedList<String>(value)
+        }
+    }
+}
+
+fun Headers.header(data: Headers, replace: Boolean = false) {
     if (data.headers.isEmpty()) return
-    headers.putAll(data.headers)
+    if (replace) {
+        headers.clear()
+        headers.putAll(data.headers)
+    } else {
+        data.headers.forEach { entry ->
+            (headers[entry.key] ?: LinkedList<String>().apply { headers[entry.key] = this })
+                .addAll(entry.value)
+        }
+    }
 }
 
 fun Headers.appendHeaders(builder: okhttp3.Request.Builder): okhttp3.Request.Builder {
     if (headers.isEmpty()) return builder
     return builder.headers(okhttp3.Headers.Builder().apply {
-        for (header in headers) add(header.key, header.value)
+        headers.forEach { entry -> entry.value.forEach { add(entry.key, it) } }
     }.build())
 }
 
 fun Headers.clearHeaders() {
     headers.clear()
+}
+
+fun Headers.clearHeaders(name: String) {
+    headers[name]?.clear()
 }
